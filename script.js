ee9430b56c9ff0e35a59bb6b7f27e817baf46726
Courtney Seto <courtneyjseto@gmail.com>
2020-02-06 21:30:38 -0800

    added style link

--- script.js
+++ script.js
@@ -37,7 +37,8 @@ inquirer
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Document</title>
+            <title>${response.username} Profile</title>
+            <link rel="stylesheet" href="./style.css"></link>
         </head>
         <body>
             <header>
